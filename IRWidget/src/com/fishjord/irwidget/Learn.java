efea0a5184651689e0ce50564990483e86fe0017
qs371102 <qs371102@163.com>
2015-12-04 19:01:04 +0800

    modify

--- IRWidget/src/com/fishjord/irwidget/Learn.java
+++ IRWidget/src/com/fishjord/irwidget/Learn.java
@@ -6,21 +6,16 @@ import com.fishjord.irwidget.ir.codes.IRCommand;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.util.Log;
-import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.Toast;
+
 
 public class Learn extends Activity {
     
 	private NetworkService service;
+	private int[] cmdAddress=new int[]{0x00};;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -31,13 +26,8 @@ public class Learn extends Activity {
 
 			@Override
 			public void onClick(View view) {
-//				IRCommand irCommand = new IRCommand(9600, new int[]{0x88,0x00,0x00,0x00,0x88});
-//				Log.d(this.getClass().getCanonicalName(),
-//						"btnLearn" + " pushed, sending "
-//								+ irCommand);
-				//service.sendCommand(irCommand);
 				int cmd=0x88;
-				int[] datas=new int[]{0x02};
+				int[] datas=cmdAddress;
 				ControlCommand command=new ControlCommand(cmd, datas,true);
 				service.sendControlCommand(command);
 			}
@@ -48,13 +38,37 @@ public class Learn extends Activity {
 
 			@Override
 			public void onClick(View view) {
-				IRCommand irCommand = new IRCommand(9600, new int[]{0x86,0x00,0x00,0x00,0x86});
-				Log.d(this.getClass().getCanonicalName(),
-						"btnLearn" + " pushed, sending "
-								+ irCommand);
-				service.sendCommand(irCommand);
+				int cmd=0x86;
+				int[] datas=cmdAddress;
+				ControlCommand command=new ControlCommand(cmd, datas,true);
+				service.sendControlCommand(command);
 			}
 		}); 
+		
+		Button btnExport=(Button)findViewById(R.id.btnExport);
+		btnExport.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				int cmd=0x89;
+				int[] datas=cmdAddress;
+				ControlCommand command=new ControlCommand(cmd, datas,true);
+				service.sendControlCommand(command);
+			}
+		});
+		
+		Button btnDownload=(Button)findViewById(R.id.btnDownload);
+		btnDownload.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				int cmd=0x8A;
+				int[] datas=new int[]{0x00};
+				ControlCommand command=new ControlCommand(cmd, datas,false);
+				service.sendControlCommand(command);
+			}
+		});
 	}
 
 }
